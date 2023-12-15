c1da08982d400a579ce11356acb6bf2ff80fd2c9
cptainzou <152598205+cptainzou@users.noreply.github.com>
2023-12-15 09:51:01 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,21 +7,23 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     PlanetX_AILens.learnObject(PlanetX_AILens.learnID.ID3)
 })
+PlanetX_AILens.ClearlearnObject()
 PlanetX_AILens.initModule()
 PlanetX_AILens.switchfunc(PlanetX_AILens.FuncList.Things)
+PlanetX_AILens.ClearlearnObject()
 basic.forever(function () {
-    neZha.setMotorSpeed(neZha.MotorList.M1, 25)
-    neZha.setMotorSpeed(neZha.MotorList.M4, 25)
     PlanetX_AILens.cameraImage()
     if (PlanetX_AILens.objectCheck(PlanetX_AILens.learnID.ID1)) {
-        neZha.stopAllMotor()
+        neZha.setMotorSpeed(neZha.MotorList.M1, 0)
+        neZha.setMotorSpeed(neZha.MotorList.M1, 0)
     } else if (PlanetX_AILens.objectCheck(PlanetX_AILens.learnID.ID3)) {
         neZha.setMotorSpeed(neZha.MotorList.M1, 15)
         neZha.setMotorSpeed(neZha.MotorList.M4, 0)
-        basic.showNumber(3)
     } else if (PlanetX_AILens.objectCheck(PlanetX_AILens.learnID.ID5)) {
         neZha.setMotorSpeed(neZha.MotorList.M1, 0)
         neZha.setMotorSpeed(neZha.MotorList.M4, 15)
-        basic.showNumber(5)
+    } else {
+        neZha.setMotorSpeed(neZha.MotorList.M1, 25)
+        neZha.setMotorSpeed(neZha.MotorList.M4, 25)
     }
 })
