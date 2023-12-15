d621e25fe7c6790c6b21c79357246acad3939dd1
cptainzou <152598205+cptainzou@users.noreply.github.com>
2023-12-15 08:52:37 +0100

    Mise à jour pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,48 +1,45 @@
 def on_button_pressed_a():
+    PlanetX_AILens.clearlearn_object()
     PlanetX_AILens.learn_object(PlanetX_AILens.learnID.ID1)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
-    PlanetX_AILens.learn_object(PlanetX_AILens.learnID.ID3)
+    PlanetX_AILens.clearlearn_object()
+    PlanetX_AILens.learn_object(PlanetX_AILens.learnID.ID5)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
-    PlanetX_AILens.learn_object(PlanetX_AILens.learnID.ID2)
+    PlanetX_AILens.clearlearn_object()
+    PlanetX_AILens.learn_object(PlanetX_AILens.learnID.ID3)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-j = 0
-i = 0
+PlanetX_Display.led_brightness(PlanetX_Display.DigitalRJPin.J2, True)
+PlanetX_AILens.clearlearn_object()
 PlanetX_AILens.init_module()
 PlanetX_AILens.switchfunc(PlanetX_AILens.FuncList.THINGS)
-PlanetX_AILens.switchfunc(PlanetX_AILens.FuncList.FACE)
 
 def on_forever():
-    global i, j
-    neZha.set_motor_speed(neZha.MotorList.M1, 50)
-    neZha.set_motor_speed(neZha.MotorList.M4, 50)
     PlanetX_AILens.camera_image()
-    if PlanetX_AILens.object_check(PlanetX_AILens.learnID.ID1) < 150:
-        if PlanetX_AILens.object_check(PlanetX_AILens.learnID.ID2) < 150:
-            if PlanetX_AILens.object_check(PlanetX_AILens.learnID.ID3) < 150:
-                if PlanetX_AILens.object_check(PlanetX_AILens.learnID.ID1):
-                    neZha.set_motor_speed(neZha.MotorList.M1, 0)
-                    neZha.set_motor_speed(neZha.MotorList.M4, 0)
-                    basic.show_number(1)
-                if PlanetX_AILens.object_check(PlanetX_AILens.learnID.ID2):
-                    neZha.set_motor_speed(neZha.MotorList.M1, 15)
-                    neZha.set_motor_speed(neZha.MotorList.M4, 15)
-                    basic.show_number(2)
-                if PlanetX_AILens.object_check(PlanetX_AILens.learnID.ID3):
-                    neZha.set_motor_speed(neZha.MotorList.M1, 15)
-                    neZha.set_motor_speed(neZha.MotorList.M4, 15)
-                    basic.show_number(3)
-    elif PlanetX_AILens.check_face():
-        if PlanetX_AILens.face_data(PlanetX_AILens.Facestatus.X) < 90:
-            i += -2
-        if PlanetX_AILens.face_data(PlanetX_AILens.Facestatus.X) > 150:
-            i += 2
-        if PlanetX_AILens.face_data(PlanetX_AILens.Facestatus.Y) > 150:
-            j += -2
-        if PlanetX_AILens.face_data(PlanetX_AILens.Facestatus.Y) < 90:
-            j += 2
+    neZha.set_motor_speed(neZha.MotorList.M1, 25)
+    neZha.set_motor_speed(neZha.MotorList.M4, 25)
+    PlanetX_Display.led_brightness(PlanetX_Display.DigitalRJPin.J2, False)
+    PlanetX_Display.led_brightness(PlanetX_Display.DigitalRJPin.J3, True)
+    if PlanetX_AILens.object_check(PlanetX_AILens.learnID.ID1):
+        PlanetX_Display.led_brightness(PlanetX_Display.DigitalRJPin.J2, True)
+        PlanetX_Display.led_brightness(PlanetX_Display.DigitalRJPin.J3, False)
+        neZha.set_motor_speed(neZha.MotorList.M1, 0)
+        neZha.set_motor_speed(neZha.MotorList.M4, 0)
+        basic.show_number(1)
+    if PlanetX_AILens.object_check(PlanetX_AILens.learnID.ID3):
+        PlanetX_Display.led_brightness(PlanetX_Display.DigitalRJPin.J2, False)
+        PlanetX_Display.led_brightness(PlanetX_Display.DigitalRJPin.J3, True)
+        neZha.set_motor_speed(neZha.MotorList.M1, 15)
+        neZha.set_motor_speed(neZha.MotorList.M4, 0)
+        basic.show_number(3)
+    if PlanetX_AILens.object_check(PlanetX_AILens.learnID.ID5):
+        PlanetX_Display.led_brightness(PlanetX_Display.DigitalRJPin.J2, False)
+        PlanetX_Display.led_brightness(PlanetX_Display.DigitalRJPin.J3, True)
+        neZha.set_motor_speed(neZha.MotorList.M1, 0)
+        neZha.set_motor_speed(neZha.MotorList.M4, 15)
+        basic.show_number(5)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -7,14 +7,19 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     PlanetX_AILens.learnObject(PlanetX_AILens.learnID.ID3)
 })
+PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J2, true)
+PlanetX_AILens.ClearlearnObject()
 PlanetX_AILens.initModule()
 PlanetX_AILens.switchfunc(PlanetX_AILens.FuncList.Things)
+PlanetX_AILens.ClearlearnObject()
+neZha.setMotorSpeed(neZha.MotorList.M1, 25)
+neZha.setMotorSpeed(neZha.MotorList.M4, 25)
 basic.forever(function () {
-    neZha.setMotorSpeed(neZha.MotorList.M1, 25)
-    neZha.setMotorSpeed(neZha.MotorList.M4, 25)
     PlanetX_AILens.cameraImage()
     if (PlanetX_AILens.objectCheck(PlanetX_AILens.learnID.ID1)) {
-        neZha.stopAllMotor()
+        neZha.setMotorSpeed(neZha.MotorList.M1, 0)
+        neZha.setMotorSpeed(neZha.MotorList.M4, 0)
+        basic.showNumber(1)
     } else if (PlanetX_AILens.objectCheck(PlanetX_AILens.learnID.ID3)) {
         neZha.setMotorSpeed(neZha.MotorList.M1, 15)
         neZha.setMotorSpeed(neZha.MotorList.M4, 0)
@@ -23,5 +28,8 @@ basic.forever(function () {
         neZha.setMotorSpeed(neZha.MotorList.M1, 0)
         neZha.setMotorSpeed(neZha.MotorList.M4, 15)
         basic.showNumber(5)
+    } else {
+        neZha.setMotorSpeed(neZha.MotorList.M1, 25)
+        neZha.setMotorSpeed(neZha.MotorList.M4, 25)
     }
 })
